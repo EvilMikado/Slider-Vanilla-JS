acd9cfce1162af6e7d01e2f37d6a390624d9b11a
EvilMikado <evil.mikado.sultan@gmail.com>
2023-05-07 16:08:16 +0300

    infinite scroll, autoplay

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const carousel = document.querySelector('.carousel')
 const arrowBtns = document.querySelectorAll('.arrow')
-let isDragging = false, startX, startScrollLeft
-const firstCardWidth = document.querySelector('.carousel__card').offsetWidth + 16;
+let isDragging = false, startX, startScrollLeft, interval
+const firstCardWidth = document.querySelector('.carousel__card').offsetWidth;
+
+const carouselChildrens = [...carousel.children]
+
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth)
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+  carousel.insertAdjacentHTML('afterbegin', card.outerHTML)
+})
+
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+  carousel.insertAdjacentHTML('beforeend', card.outerHTML)
+})
 
 arrowBtns.forEach(btn => {
   btn.addEventListener('click', ()=>{
@@ -26,6 +38,29 @@ const dragStop = () =>{
   carousel.classList.remove('dragging')
 }
 
+const infiniteScroll = () =>{
+  if (carousel.scrollLeft === 0) {
+    carousel.classList.add('no-smooth-scroll')
+    carousel.scrollLeft =  carousel.scrollWidth - (2 * carousel.offsetWidth)
+    carousel.classList.remove('no-smooth-scroll')
+  }else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth -carousel.offsetWidth){
+    carousel.classList.add('no-smooth-scroll')
+    carousel.scrollLeft = carousel.offsetWidth
+    carousel.classList.remove('no-smooth-scroll')
+  }
+}
+
+const autoPlay = () =>{
+  if (window.innerWidth < 800) return
+  interval = setInterval(()=>{
+    carousel.scrollLeft += firstCardWidth
+  }, 3000)
+}
+
+
+ autoPlay()
+carousel.addEventListener('mouseover', () => clearInterval(interval))
 carousel.addEventListener('mousemove', dragging)
 carousel.addEventListener('mousedown', dragStart)
-carousel.addEventListener('mouseup', dragStop)
+document.addEventListener('mouseup', dragStop)
+carousel.addEventListener('scroll', infiniteScroll)
